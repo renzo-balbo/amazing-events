72c6ee86927db82a7e0e3e50d8b0aabe49d5caf1
renzo-balbo <106273636+renzo-balbo@users.noreply.github.com>
2022-07-02 17:48:46 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,26 +1,23 @@
 const URI = 'http://amazing-events.herokuapp.com/api/events'
 let dataDeEventos = []
 
-function cargarDatos(URL){
-    fetch(URL)
-        .then (respuesta => respuesta.json())
-            .then(data =>{
-                let eventos = data.events
-                creacionDeCheckboxes(filtrarAlfabeticamente(eventos))
-                impresionDeTarjetas(filtrarAlfabeticamente(eventos))
-                dataDeEventos = filtrarAlfabeticamente(eventos)
-            })
-    }
-
-cargarDatos(URI)
-
 // VARIABLES
 
 let barraDeBusqueda = document.getElementById('barraDeBusqueda');
 let contenedorDeCheckboxes = document.getElementById('filterContainer');
-let cardContainer = document.getElementById('cardContainer')
+let cardContainer = document.getElementById('cardContainer');
 
-barraDeBusqueda.addEventListener('keyup', ()=>{
+let index = document.getElementById('index');
+let upcomingEvents = document.getElementById('upcomingEvents');
+let pastEvents = document.getElementById('pastEvents');
+let cajaDetalles = document.getElementById('detailsBox')
+
+const queryString = location.search;
+const params = new URLSearchParams(queryString);
+const id = params.get("id");
+
+if (cardContainer) {
+    barraDeBusqueda.addEventListener('keyup', ()=>{
     let primerFiltrado = filtrarPorInputDeTexto(dataDeEventos, barraDeBusqueda.value)
     let segundoFiltrado = filtrarPorCheckbox(primerFiltrado)
     impresionDeTarjetas(segundoFiltrado, cardContainer)
@@ -31,9 +28,9 @@ contenedorDeCheckboxes.addEventListener('change', () =>{
     let segundoFiltrado = filtrarPorInputDeTexto(primerFiltrado, barraDeBusqueda.value)
     impresionDeTarjetas(segundoFiltrado, cardContainer);
 })
+}
 
 // FUNCIONES
-
 function filtrarAlfabeticamente(eventos){
     eventos.sort((firstCard, secondCard) => {
         if (firstCard.name > secondCard.name) {
@@ -62,7 +59,7 @@ function filtrarPorCheckbox(arrayDeEventos){
 }
 
 function filtrarPorInputDeTexto(arrayDeEventos, texto){
-    let arrayFiltrado = arrayDeEventos.filter(evento => evento.name.toLowerCase().includes(texto.toLowerCase()))
+    let arrayFiltrado = arrayDeEventos.filter(evento => evento.name.toLowerCase().includes(texto.toLowerCase()) || evento.description.toLowerCase().includes(texto.toLowerCase()))
     return arrayFiltrado
 }
 
@@ -113,3 +110,122 @@ function creacionDeCheckboxes(arrayDeEventos){
     contenedorDeCheckboxes.appendChild(div);
     })
 }
+
+function cargarDatos(URL){
+    fetch(URL)
+        .then (respuesta => respuesta.json())
+            .then(data =>{
+                let eventos = [];
+                if (index){
+                    eventos = data.events
+                }
+                if (upcomingEvents){
+                    eventos = data.events.filter((evento)=> evento.date > data.currentDate)
+                }
+                if (pastEvents){
+                    eventos = data.events.filter((evento)=> evento.date < data.currentDate)
+                }
+                let eventosOrdenados = filtrarAlfabeticamente(eventos)
+                if (cardContainer){
+                creacionDeCheckboxes(eventosOrdenados)
+                impresionDeTarjetas(eventosOrdenados)
+                dataDeEventos = eventosOrdenados
+                }
+                if (cajaDetalles){
+                    eventos = data.events
+                    let eventoSeleccionado = encontrarEvento(eventos)
+                    crearDetalles(eventoSeleccionado, cajaDetalles)
+                }
+                if (tabla){
+                    let futuros = data.events.filter((evento)=> evento.date > data.currentDate)
+                    let pasados = data.events.filter((evento)=> evento.date < data.currentDate)
+                    rellenarTabla(pasados)
+                    crearRenglon(futuros, upcompingStats)
+                    crearRenglon(pasados, pastStats)
+                }
+            })
+}
+
+// Details
+function encontrarEvento(eventos){
+    let selectedEvent = eventos.find((events) => events._id == id);
+    return selectedEvent
+}
+
+function crearDetalles(event, lugarDeImpresion) {
+    let div = document.createElement("div");
+    div.className =
+    "d-flex flex-wrap col-9 p-3 fondoVerde detailsBox justify-content-evenly align-items-center";
+    div.innerHTML = `
+    <div class="col-12 col-md-6 d-flex justify-content-center">
+    <img class="img-fluid col-10" src="${event.image}" alt="food_fair">
+</div>
+
+<div class="col-12 col-md-6">
+    <div class="text-black d-flex flex-wrap justify-content-center">
+        <h5 class="col-12 text-center">${event.name}</h5>
+        <h6 class="col-12 rosa text-center">${event.date}</h6>
+        <p class="col-12 text-center">${event.description}</p>
+            <p class="col-12 text-center text-black">Category: ${event.category}</p>
+            <p class="col-12 text-center text-black">Place: ${event.place}</p>
+            <p class="col-12 text-center text-black">Capacity: ${event.capacity}</p>
+            <p class="col-12 text-center text-black">${event.assistance ? 'Assistance' : 'Estimate'}: ${event.assistance ? event.assistance : event.estimate}</p>
+            <p class="col-12 text-center text-black">Price: $${event.price}</p>
+    </div>
+</div>
+    `;
+    lugarDeImpresion.appendChild(div);
+}
+
+// Tabla
+let tabla = document.getElementById('table');
+let maximoGeneral = document.getElementById('maximoGeneral');
+let minimoGeneral = document.getElementById('minimoGeneral');
+let mayorCapacidad = document.getElementById('mayorCapacidad');
+let upcompingStats = document.getElementById('upcomingStats');
+let pastStats = document.getElementById('pastStats');
+
+function rellenarTabla (eventos){
+let capacidad = eventos.map(event => event.capacity)
+let eventMaxCapacidad = eventos.find(event => event.capacity == Math.max(...capacidad))
+
+let asistencia = eventos.map(event => event.assistance / event.capacity)
+let eventMaxAsistencia = eventos.find(event => event.assistance / event.capacity == Math.max(...asistencia))
+let eventMinAsistencia = eventos.find(event => event.assistance / event.capacity == Math.min(...asistencia))
+
+maximoGeneral.innerText = `${eventMaxAsistencia.name}`
+minimoGeneral.innerText = `${eventMinAsistencia.name}`
+mayorCapacidad.innerText = `${eventMaxCapacidad.name}`
+}
+
+function crearRenglon(eventos, tbody){
+    let listaDeCategorias = []
+    eventos.forEach(evento =>{
+        if (!listaDeCategorias.includes(evento.category)){
+            listaDeCategorias.push(evento.category)
+        }
+    })
+    listaDeCategorias.forEach(categoria =>{
+        let estimados = []
+        let capacidad = []
+        let arrayDeGanancias = []
+
+        eventos.forEach(evento =>{
+            if (evento.category == categoria){
+                estimados.push(evento.estimate? evento.estimate : evento.assistance)
+                capacidad.push(evento.capacity)
+                arrayDeGanancias.push(evento.price* Number(evento.estimate? evento.estimate : evento.assistance))
+            }
+        })
+        let renglon = document.createElement('tr')
+        renglon.innerHTML =`
+        <td>${categoria}</td>
+        <td>$${arrayDeGanancias.reduce((a, b) => a + b)}</td>
+        <td>${Math.round((estimados.map(i=>Number(i)).reduce((a, b) => a + b))*100/(capacidad.map(i=>Number(i)).reduce((a, b) => a + b)))}%</td>
+        `
+        tbody.appendChild(renglon)
+    })
+}
+
+//--------------------------------------------------------
+cargarDatos(URI)
